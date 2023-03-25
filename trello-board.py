48ea6d17b67651f9923daaa5724daccbc4eb6530
NgoHaiLOng <ngohailong1991@gmail.com>
2023-03-25 11:22:28 +0700

    'fixed'
    Signed-off-by: NgoHaiLOng <ngohailong1991@gmail.com>

--- trello-board.py
+++ trello-board.py
@@ -2,8 +2,7 @@
 # coding: utf-8
 
 __doc__ = """
-Yêu cầu:
-Viết script dùng API tạo 1 Trello board với 2 list "Thứ 3", "Thứ 5",
+script dùng API tạo 1 Trello board với 2 list "Thứ 3", "Thứ 5",
 và tạo 12 card ứng với 12 buổi học của lớp, có set due date ứng với các ngày
 học.
 
